6de456e90b9e294f17b85d92e18bc9753a60586e
Hunter Kievet <hkievet@gmail.com>
2022-06-27 12:32:21 -0500

    basic app...

--- content-script.js
+++ content-script.js
@@ -1,10 +1,26 @@
-const imgs = document.getElementsByTagName("img")
-const imgsAry = [...imgs]
+let imgs = document.getElementsByTagName("img")
+let imgsAry = [...imgs]
 
 
-const src = imgsAry.map(img => {
+let src = imgsAry.map(img => {
     return img.src
 });
-
 src;
 
+
+function handleResponse(message) {
+    console.log(`Message from the background script:  ${message.response}`);
+}
+
+function handleError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function notifyBackgroundPage(e) {
+    let sending = browser.runtime.sendMessage({
+        greeting: e
+    });
+    sending.then(handleResponse, handleError);
+}
+
+notifyBackgroundPage(src)
---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    function printMessage(message) {
+        console.log(message);
+    }
+    function handleMessage(request, sender, sendResponse) {
+        console.log("Message from the content script: " + request.greeting);
+        sendResponse({ response: "Response from background script" });
+    }
+
+    browser.runtime.onMessage.addListener(handleMessage);
+</script>
+
+<svelte:window on:scraper_images_scraped={printMessage} />
+<p>Bitching</p>
